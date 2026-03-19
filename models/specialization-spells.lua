79a11b5d5d468446c0ec1c2f6d3e3a613ead9f0a
Stefan Schulze <stefan.schulze@evas.de>
2026-03-19 16:27:50 +0100

    Added new spec view.

--- models/specialization-spells.lua
+++ models/specialization-spells.lua
@@ -10,33 +10,33 @@
 _G.professionMaster:CreateModel("specialization-spells", {
     -- Alchemy (171)
     [171] = {
-        { spellId = 28675 },
-        { spellId = 28677 },
-        { spellId = 28672 },
+        { spellId = 28675, icon = 134715 },
+        { spellId = 28677, icon = 134876 },
+        { spellId = 28672, icon = 134918 },
     },
     -- Blacksmithing (164)
     [164] = {
-        { spellId = 9788 },
-        { spellId = 9787 },
-        { spellId = 17039 },
-        { spellId = 17040 },
-        { spellId = 17041 },
+        { spellId = 9788, icon = 132739 },
+        { spellId = 9787, icon = 135326 },
+        { spellId = 17039, icon = 135351 },
+        { spellId = 17040, icon = 133060 },
+        { spellId = 17041, icon = 132396 },
     },
     -- Leatherworking (165)
     [165] = {
-        { spellId = 10656 },
-        { spellId = 10658 },
-        { spellId = 10660 },
+        { spellId = 10656, icon = 134305 },
+        { spellId = 10658, icon = 135830 },
+        { spellId = 10660, icon = 136069 },
     },
     -- Tailoring (197)
     [197] = {
-        { spellId = 26797 },
-        { spellId = 26801 },
-        { spellId = 26798 },
+        { spellId = 26797, icon = 135880 },
+        { spellId = 26801, icon = 132888 },
+        { spellId = 26798, icon = 132895 },
     },
     -- Engineering (202)
     [202] = {
-        { spellId = 20219 },
-        { spellId = 20222 },
+        { spellId = 20219, icon = 132996 },
+        { spellId = 20222, icon = 135826 },
     },
 });
